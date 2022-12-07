8d3b767ef89d8da528ed6aaa97a3bd5e1710d96a
michaldoman <mjd.domanski@gmail.com>
2022-12-07 20:43:03 +0100

    version_1.1

--- Lotto Simulator.py
+++ Lotto Simulator.py
@@ -1,13 +1,25 @@
-from random import randint, sample, shuffle
+from random import sample, shuffle
 
 
 def main():
     count = 6
     winning_set = randomize_winning_set(count)
     user_set = take_user_input(count)
-    correct_guesses = 0
-    print(winning_set)
-    print(user_set)
+    correct_guesses = compare_lists(winning_set, user_set)
+
+    print('')
+    print(f'Your numbers: {user_set}')
+    print(f'Winning numbers: {winning_set}')
+    print('')
+
+    if correct_guesses == 0:
+        print('Unfortunately, you did not guess any of the numbers :( ')
+    elif correct_guesses == 1:
+        print(f'Congratulations! You guessed 1 number!')
+    elif 6 > correct_guesses > 1:
+        print(f'Congratulations! You guessed {correct_guesses} numbers!')
+    else:
+        print('It is UNBELIEVABLE!! You guessed all of the numbers! You win the main price!')
 
 
 def randomize_winning_set(count):
@@ -24,7 +36,7 @@ def randomize_winning_set(count):
 
 
 def take_user_input(count):
-    """Takes input from user, and makes a list of theirs guessed numbers.
+    """Takes input from user, and makes a list of their guessed numbers.
 
     :param count: an integer that states how many numbers are to be generated.
     :return: a list containing users' input
@@ -37,14 +49,30 @@ def take_user_input(count):
             num = int(input(f'{guess}: Choose a number from 1 to 49: '))
             if num < 1 or num > 49:
                 raise ValueError
+            elif num in user_set:
+                print('You have already chose that number!')
+                raise ValueError
             user_set.append(num)
             guess += 1
         except ValueError:
             print('This is not a valid entry!')
+            print('')
     return sorted(user_set)
 
 
+def compare_lists(list_1, list_2):
+    """Check how many values are shared by two different lists.
 
+    :param list_1: first set of values
+    :param list_2: second set of values
+    :return: an integer: a number of values that repeat for both lists
+    """
+
+    repeats = 0
+    for i in list_1:
+        if i in list_2:
+            repeats += 1
+    return repeats
 
 
 if __name__ == '__main__':
